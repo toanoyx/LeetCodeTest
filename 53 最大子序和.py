894000ea1945c1ca69c5427e9870e652a2e3c9d8
toanoyx <38813080+toanoyx@users.noreply.github.com>
2020-01-13 09:22:06 +0800

    Create 53 最大子序和.py

---
+++ 53 最大子序和.py
@@ -0,0 +1,9 @@
+# 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+# 输入：[-2,1,-3,4,-1,2,1,-5,4]
+# 输出：6
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        for i in range(1, len(nums)):
+            nums[i] = nums[i] + max(nums[i-1], 0)
+        return max(nums)
